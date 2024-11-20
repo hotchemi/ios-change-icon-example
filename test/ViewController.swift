0057136fd648dedf390cfc46af5324ad8e5de6f4
Shintaro Katafuchi <shintaro.katafuchi.eyi@jmdc.co.jp>
2024-11-20 15:46:30 +0900

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by shintaro.katafuchi.eyi on 2024/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
